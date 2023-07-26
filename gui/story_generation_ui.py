cb933032202ba809d1dccc914b56bc62e9e8a683
Adithya S K <adithyaskolavi@gmail.com>
2023-07-26 11:40:12 +0530

    added story generation UI - Adithya S K

--- gui/story_generation_ui.py
+++ gui/story_generation_ui.py
@@ -5,11 +5,6 @@ from storyblocks.api_utils.eleven_api import getVoices
 from storyblocks.config.languages import Language
 import time
 
-import gradio as gr
-import random
-import os
-import time
-
 language_choices = [lang.value.upper() for lang in Language]
 
 ERROR_TEMPLATE = """
@@ -31,15 +26,18 @@ def getElevenlabsVoices():
 
 voiceChoice = gr.Radio(getElevenlabsVoices(), label="Elevenlabs voice", value="Antoni", interactive=True)
 
-def create_short_automation_ui(StoryBlocksUI: gr.Blocks):
-    with gr.Row(visible=True) as story_generation:
-        with gr.Column():
-            assistance = gr.Radio(["AI Generated Story","Custom Story"], label="Type of story generated", value="AI Generated Story", interactive=True)
-            prompt = gr.Textbox(label="Prompt", lines=5, placeholder="Write a prompt for your story", interactive=True , visible=True)
-            story = gr.Textbox(label="Custom Story", lines=10, placeholder="Write your Custom Story", interactive=True , visible=False)
-            assistance.change(lambda x: (gr.update(visible= x == assistance.choices[0]), gr.update(visible= x == assistance.choices[1])), [assistance], [prompt, story])
-            genre = gr.Textbox(label="Genre", placeholder="Write a genre for your story", interactive=True, visible=True)
-            duration = gr.Slider(minimum=0, maximum=2, step=.2, label="Duration in minutes", value=2, interactive=True, visible=True)
-            language = gr.Radio(language_choices, label="Language", value="ENGLISH")
-            createButton = gr.Button(label="Generate Story", size="sm", type="submit", interactive=True, visible=True)
+
+def story_generation_ui(StoryBlocksUI: gr.Blocks):
+    with gr.Tab("Generate Story") as  story_generation:
+        gr.Markdown("## Generate Story") 
+        with gr.Row(visible=True):
+            with gr.Column():
+                assistance = gr.Radio(["AI Generated Story","Custom Story"], label="Type of story generated", value="AI Generated Story", interactive=True)
+                prompt = gr.Textbox(label="Prompt", lines=5, placeholder="Write a prompt for your story", interactive=True , visible=True)
+                story = gr.Textbox(label="Custom Story", lines=10, placeholder="Write your Custom Story", interactive=True , visible=False)
+                assistance.change(lambda x: (gr.update(visible= x == assistance.choices[0]), gr.update(visible= x == assistance.choices[1])), [assistance], [prompt, story])
+                genre = gr.Textbox(label="Genre", placeholder="Write a genre for your story", interactive=True, visible=True)
+                duration = gr.Slider(minimum=0, maximum=2, step=.2, label="Duration in minutes", value=2, interactive=True, visible=True)
+                language = gr.Radio(language_choices, label="Language", value="ENGLISH")
+                createButton = gr.Button(label="Generate Story", size="sm", type="submit", interactive=True, visible=True)
     return story_generation
