8e1b229cd3f6b9a25fa1c2b4a2fa1aea60b8512a
Adithya S K <adithyaskolavi@gmail.com>
2023-09-16 01:32:37 +0530

    project selection and updating complete - Adithya S K

--- gui/project_selection_ui.py
+++ gui/project_selection_ui.py
@@ -1,9 +1,10 @@
 import os
+import time
 import gradio as gr
 from storyblocks.config.api_db import get_api_key
 from storyblocks.config.project_db import get_project_name, set_project_name
 
-def create_project(project_name, project_description):
+def Create_project(project_name, project_description):
     if not project_name:
         raise gr.Error("Please write down your project's name")
     elif not project_description:
@@ -19,29 +20,36 @@ def create_project(project_name, project_description):
             with open(f"projects/{project_name}/content.json", "w") as f:
                 f.write(f"""{{ "name": "{project_name}", "description": "{project_description}" }}""")
             set_project_name(project_name)
-    return gr.update(visible=True)
 
+def Select_project(project_name_input):
+    if not project_name_input:
+        raise gr.Error("Please select a project")    
+    set_project_name(project_name_input)
+    return f"{project_name_input}"
+
+def Update_projects_list(name):
+    new_project = os.listdir("projects")
+    return gr.update(choices=new_project , value=name)
+
+def get_current_project():
+    if get_project_name() not in os.listdir("projects"):
+        set_project_name("")
+        return ""
+    return get_project_name()
 
 def select_project_ui(StoryBlocksUI: gr.Blocks):
-    with gr.Tab("Create Project") as select_project:
+    with gr.Tab("Manage Projects") as select_project:
+        current_project = gr.Textbox(value=get_current_project(), label="Current Project", interactive=False, visible=True)
         with gr.Row(visible=True):
             with gr.Column():
-                project_name = gr.Textbox(label="Project Name", placeholder="Write a name for your project", interactive=True, visible=True)
+                project_name = gr.Textbox(label="Project Name",placeholder="Project Name" ,interactive=True, visible=True)
                 project_description = gr.Textbox(label="Project Description", placeholder="Write a description for your project", interactive=True, visible=True)
-                # create_project_button = gr.Button(label="Create Project", size="sm", type="submit", interactive=True, visible=True)
-                with gr.Row():
-                    button_project = gr.Button("Create Project", size="sm", interactive=True, visible=True ,)
-
-        with gr.Column():
-            
-            gr.Markdown("## Previous Projects")
-            list_of_projects = os.listdir("projects")
-            with gr.Row():
-                for i in range(len(list_of_projects)):
-                    gr.Button(list_of_projects[i], size="sm" , interactive=True, visible=True)
-
-            
-        generation_error = gr.HTML(visible=False)
-        button_project.click(create_project , inputs=[project_name, project_description])
-
+                create_project_button = gr.Button("Create Project", size="sm", interactive=True, visible=True )
+            with gr.Column():
+                existing_projects = os.listdir("projects")
+                existing_projects_options = gr.Dropdown(existing_projects,label="Select Existing Project",)
+                existing_projects_options.select(Select_project, inputs=existing_projects_options , outputs=current_project)
+                create_project_button.click(Create_project, inputs=[project_name, project_description])
+                create_project_button.click(Select_project, inputs=project_name , outputs=current_project)
+                create_project_button.click(Update_projects_list, inputs=project_name , outputs=existing_projects_options)
     return select_project
