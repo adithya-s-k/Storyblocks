4083e4ea7d7a17ccdfd1fe5b3963f3580307fcf4
Adithya S K <adithyaskolavi@gmail.com>
2023-09-17 04:16:43 +0530

    implemented voice output in voice generation ui - Adithya S K

--- gui/voice_generation_ui.py
+++ gui/voice_generation_ui.py
@@ -1,6 +1,11 @@
+import os
+import json
+import pyttsx3
 import gradio as gr
 from storyblocks.config.api_db import get_api_key
 from storyblocks.api_utils.eleven_api import getVoices
+from storyblocks.config.project_db import get_project_name, set_project_name
+from elevenlabs import generate, set_api_key , save
 
 def getElevenlabsVoices():
     api_key = get_api_key("ELEVEN LABS")
@@ -9,18 +14,60 @@ def getElevenlabsVoices():
 
 voiceChoice = gr.Radio(getElevenlabsVoices(), label="Elevenlabs voice", value="Antoni", interactive=True)
 
+def generateVoice_ElevenLabs(selected_voice):
+  set_api_key(get_api_key("ELEVEN LABS"))
+
+  current_selected_project = get_project_name()
+  
+  with open(f"projects/{current_selected_project}/content.json", "r") as f:
+    content = json.load(f)
+    story = content["content"]
+    
+  print(story)
+
+  audio = generate(
+    text=story,
+    voice=selected_voice
+  )
+  
+  audio_file_path = f"projects/{current_selected_project}/audio.mp3"
+  save(audio, audio_file_path)
+  
+  return  gr.update(value=f"projects/{current_selected_project}/audio.mp3")
+
+def generateVoice_PythonTTS():
+  current_selected_project = get_project_name()
+  
+  with open(f"projects/{current_selected_project}/content.json", "r") as f:
+    content = json.load(f)
+    story = content["content"]
+    
+  print(story)
+  engine = pyttsx3.init()
+  
+  engine.save_to_file(story, f"projects/{current_selected_project}/audio.mp3")
+  engine.runAndWait()
+  
+  return  gr.update(value=f"projects/{current_selected_project}/audio.mp3")
+
+
 def elevenLabsUI(StoryBlocksUI: gr.Blocks):
   with gr.Row(visible=False) as eleven_labs_ui:
     with gr.Column():
       gr.Markdown("## Elevenlabs Voice")
       voiceChoice.render()
-      button = gr.Button("Generate Voice", size="sm", interactive=True, visible=True)
+      elevenlab_generate_button = gr.Button("Generate Voice", size="sm", interactive=True, visible=True)
+      generated_audio = gr.Audio(label="Audio", interactive=True, visible=False)
+      elevenlab_generate_button.click(lambda : gr.update(visible=True), outputs=[generated_audio]).success(generateVoice_ElevenLabs ,inputs=[voiceChoice] , outputs=[generated_audio])
   return eleven_labs_ui
 
 def pythonTTSUI(StoryBlocksUI: gr.Blocks):
   with gr.Row(visible=False) as python_tts_ui:
-    gr.Markdown("## Python TTS")
-    button = gr.Button("Generate Voice", size="sm", interactive=True, visible=True)
+    with gr.Column():
+      gr.Markdown("## Python TTS")
+      pyttsx3_generate_button = gr.Button("Generate Voice", size="sm", interactive=True, visible=True)
+      generated_audio = gr.Audio(label="Audio", interactive=True, visible=False)
+      pyttsx3_generate_button.click(lambda : gr.update(visible=True), outputs=[generated_audio]).success(generateVoice_PythonTTS , outputs=[generated_audio])
   return python_tts_ui
     
 
